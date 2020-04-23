69060b8cdff19e14f85094661e99acad17bec87a
Ksinia <xiatian@mail.ru>
2020-04-23 04:30:11 +0200

    Add high score to score label

--- src/ui/ScoreLabel.js
+++ src/ui/ScoreLabel.js
@@ -1,16 +1,23 @@
 import Phaser from "phaser";
 
-const formatScore = (score) => `Score: ${score}`;
+const formatScore = (score, highScore) => `Score: ${score}\nHigh Score: ${highScore}`;
+
+const style = { fontSize: 32, fill: "#fff" };
 
 export default class ScoreLabel extends Phaser.GameObjects.Text {
-  constructor(scene, x, y, score, style) {
-    super(scene, x, y, formatScore(score), style);
+  constructor(scene, x, y, score, highScore) {
+    super(scene, x, y, formatScore(score, highScore), style);
 
     this.score = score;
+    this.highScore = highScore;
   }
 
   setScore(score) {
     this.score = score;
+    this.updateScoreText();
+  }
+  setHighScore(highScore) {
+    this.highScore = highScore;
     this.updateScoreText();
   }
 
@@ -19,6 +26,6 @@ export default class ScoreLabel extends Phaser.GameObjects.Text {
   }
 
   updateScoreText() {
-    this.setText(formatScore(this.score));
+    this.setText(formatScore(this.score, this.highScore));
   }
 }
